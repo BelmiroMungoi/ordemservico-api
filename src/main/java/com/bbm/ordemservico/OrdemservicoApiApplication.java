3653386330c71720b13004e82ba13dc04f6c918d
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-02 18:33:19 +0200

    classe principal do projecto

---
+++ src/main/java/com/bbm/ordemservico/OrdemservicoApiApplication.java
@@ -0,0 +1,13 @@
+package com.bbm.ordemservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemservicoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemservicoApiApplication.class, args);
+	}
+
+}
