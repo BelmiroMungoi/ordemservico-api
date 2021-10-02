f4c3a173e79f778ffa3d2f131e7f6a7a2028d5f0
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-02 19:02:38 +0200

    criando o repository cliente para a persistencia de dados

---
+++ src/main/java/com/bbm/ordemservico/domain/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.bbm.ordemservico.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bbm.ordemservico.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
