fcb9bb865555fa347ae9d52d1c2ee64124cf218f
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-02 19:03:28 +0200

    adicionando os metodos equals and hashCode para diferenciacao de objectos

--- src/main/java/com/bbm/ordemservico/domain/model/Cliente.java
+++ src/main/java/com/bbm/ordemservico/domain/model/Cliente.java
@@ -1,5 +1,7 @@
 package com.bbm.ordemservico.domain.model;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -12,13 +14,13 @@ public class Cliente {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false)
 	private String nome;
-	
+
 	@Column(nullable = false)
 	private String email;
-	
+
 	@Column(nullable = false)
 	private String telefone;
 
@@ -52,6 +54,23 @@ public class Cliente {
 
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
