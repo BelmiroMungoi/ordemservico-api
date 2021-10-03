9ae470ecae80fca31e07b9d3d95173fa50127f8a
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-03 19:13:57 +0200

    adicionando id do cliente ao criar uma ordem de servico

--- src/main/java/com/bbm/ordemservico/domain/service/GestaoOrdemServicoService.java
+++ src/main/java/com/bbm/ordemservico/domain/service/GestaoOrdemServicoService.java
@@ -5,8 +5,11 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bbm.ordemservico.domain.exceptionhandler.NegocioException;
+import com.bbm.ordemservico.domain.model.Cliente;
 import com.bbm.ordemservico.domain.model.OrdemServico;
 import com.bbm.ordemservico.domain.model.StatusOrdemServico;
+import com.bbm.ordemservico.domain.repository.ClienteRepository;
 import com.bbm.ordemservico.domain.repository.OrdemServicoRepository;
 
 @Service
@@ -15,8 +18,15 @@ public class GestaoOrdemServicoService {
 	@Autowired
 	private OrdemServicoRepository ordemServicoRepository;
 	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	public OrdemServico createOrder(OrdemServico ordemServico) {
 		
+		Cliente cliente = clienteRepository.findById(ordemServico.getCliente().getId())
+				.orElseThrow(() -> new NegocioException("Cliente não encontrado"));
+		
+		ordemServico.setCliente(cliente);
 		ordemServico.setStatus(StatusOrdemServico.ABERTA);
 		ordemServico.setDataAbertura(LocalDateTime.now());
 		
