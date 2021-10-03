589e415f260be24725f9035bbab174dc6cc3d2e9
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-03 18:43:22 +0200

    criando a entidade ordem de servico

---
+++ src/main/java/com/bbm/ordemservico/domain/model/OrdemServico.java
@@ -0,0 +1,142 @@
+package com.bbm.ordemservico.domain.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class OrdemServico implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String descricao;
+
+	@Column(nullable = false)
+	private String equipamento;
+
+	@Column(nullable = false)
+	private String defeito;
+
+	@Column(nullable = false)
+	private String solucao;
+
+	@Column(nullable = false)
+	private BigDecimal preco;
+
+	@Column(nullable = false)
+	private LocalDateTime dataAbertura;
+	
+	private LocalDateTime dataFinalizacao;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private StatusOrdemServico status;
+
+	@ManyToOne
+	private Cliente cliente;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getEquipamento() {
+		return equipamento;
+	}
+
+	public void setEquipamento(String equipamento) {
+		this.equipamento = equipamento;
+	}
+
+	public String getDefeito() {
+		return defeito;
+	}
+
+	public void setDefeito(String defeito) {
+		this.defeito = defeito;
+	}
+
+	public String getSolucao() {
+		return solucao;
+	}
+
+	public void setSolucao(String solucao) {
+		this.solucao = solucao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public StatusOrdemServico getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusOrdemServico status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDateTime getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+
+	public void setDataFinalizacao(LocalDateTime dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdemServico other = (OrdemServico) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
