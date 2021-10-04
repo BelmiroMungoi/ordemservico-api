7e317a38532d8d04ae3813f00053a64e9789dc78
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-04 19:08:43 +0200

    criando a classe para receber o id do cliente

---
+++ src/main/java/com/bbm/ordemservico/api/model/ClienteIdInput.java
@@ -0,0 +1,18 @@
+package com.bbm.ordemservico.api.model;
+
+import javax.validation.constraints.NotNull;
+
+public class ClienteIdInput {
+
+	@NotNull
+	private Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+}
