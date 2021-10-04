0cad42bc94366602caee253a997aee53ccab62e0
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-04 19:07:46 +0200

    criando um dto com o resumo dos dados do cliente

---
+++ src/main/java/com/bbm/ordemservico/api/model/ClienteResumDto.java
@@ -0,0 +1,24 @@
+package com.bbm.ordemservico.api.model;
+
+public class ClienteResumDto {
+
+	private Long idCliente;
+	private String nomeCliente;
+
+	public Long getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(Long idCliente) {
+		this.idCliente = idCliente;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+}
