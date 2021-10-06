6ee8bdd86e84de3c432228581c60f1048dce2ced
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-10-06 15:30:02 +0200

    refatorando as classes dto

---
+++ src/main/java/com/bbm/ordemservico/api/model/dtos/OrdemServicoDto.java
@@ -0,0 +1,101 @@
+package com.bbm.ordemservico.api.model.dtos;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import com.bbm.ordemservico.domain.model.enums.StatusOrdemServico;
+
+public class OrdemServicoDto {
+
+	private Long id;
+	private String descricao;
+	private String defeito;
+	private String equipamento;
+	private String solucao;
+	private BigDecimal preco;
+	private StatusOrdemServico status;
+	private OffsetDateTime dataAbertura;
+	private OffsetDateTime dataFinalizacao;
+	private ClienteResumDto cliente;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDefeito() {
+		return defeito;
+	}
+
+	public void setDefeito(String defeito) {
+		this.defeito = defeito;
+	}
+
+	public String getEquipamento() {
+		return equipamento;
+	}
+
+	public void setEquipamento(String equipamento) {
+		this.equipamento = equipamento;
+	}
+
+	public String getSolucao() {
+		return solucao;
+	}
+
+	public void setSolucao(String solucao) {
+		this.solucao = solucao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public StatusOrdemServico getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusOrdemServico status) {
+		this.status = status;
+	}
+
+	public OffsetDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(OffsetDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public OffsetDateTime getDataFinalizacao() {
+		return dataFinalizacao;
+	}
+
+	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
+		this.dataFinalizacao = dataFinalizacao;
+	}
+
+	public ClienteResumDto getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(ClienteResumDto cliente) {
+		this.cliente = cliente;
+	}
+
+}
